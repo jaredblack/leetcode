3830915dd5f1d0f1e1ba1ede058353feef3280a8
jaredblack <jrdblck@gmail.com>
2023-11-06 10:07:19 -0700

    Complete 134. Gas Station

---
+++ 134.gasstation.py
@@ -0,0 +1,18 @@
+import math
+
+
+def canCompleteCircuit(gas: list[int], cost: list[int]) -> int:
+    diff = sum(gas) - sum(cost)
+    if diff < 0:
+        return -1
+    total = 0
+    start_i = 0
+    for i in range(len(gas)):
+        total += gas[i] - cost[i]
+        if total < 0:
+            start_i = i + 1
+            total = 0
+    return start_i
+
+        
+print(canCompleteCircuit([1,2,3,4,5], [3,4,5,1,2]))       
